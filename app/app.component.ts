7a0304f5dd2f04e75e24ba71fb5f7ce75132049e
ishanRox <roxishan8@gmail.com>
2021-05-22 19:09:48 +0530

    wide ,apply for all in same button group

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,8 +11,40 @@ import { Component } from '@angular/core';
       <button kendoButton [disabled]="true">another default button</button>
     </div>
 
+    <hr />
     <button kendoButton [primary]="true">primary button</button>
     <button kendoButton [togglable]="true">togglable button</button>
+    <button kendoButton icon="calendar">icon button</button>
+
+    <hr />
+    <br />
+    <h5>button group</h5>
+    <kendo-buttongroup>
+      <button kendoButton [primary]="true">primary button</button>
+      <button kendoButton [togglable]="true">togglable button</button>
+      <button kendoButton icon="calendar">icon button</button>
+      <kendo-buttongroup> </kendo-buttongroup
+    ></kendo-buttongroup>
+
+    <hr />
+    <br />
+    <h5>button group look=outline for all</h5>
+    <kendo-buttongroup look="outline">
+      <button kendoButton [primary]="true">primary button</button>
+      <button kendoButton [togglable]="true">togglable button</button>
+      <button kendoButton icon="calendar">icon button</button>
+      <kendo-buttongroup> </kendo-buttongroup
+    ></kendo-buttongroup>
+
+    <hr />
+    <br />
+    <h5>button group width</h5>
+    <kendo-buttongroup width="100">
+      <button kendoButton [primary]="true">primary button</button>
+      <button kendoButton [togglable]="true">togglable button</button>
+      <button kendoButton icon="calendar">icon button</button>
+      <kendo-buttongroup> </kendo-buttongroup
+    ></kendo-buttongroup>
   `
 })
 export class AppComponent {
