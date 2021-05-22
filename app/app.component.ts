d5e8a34b0028bc9c9f32905bf60265888ce5a87d
ishanRox <roxishan8@gmail.com>
2021-05-22 18:44:39 +0530

    primary togglable

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,8 +7,12 @@ import { Component } from '@angular/core';
       <button kendoButton look="default">Default button</button>
       <button kendoButton look="outline">outline button</button>
       <button kendoButton look="bare">bare button</button>
-      <button kendoButton >another default button</button>
+      <button kendoButton>another default button</button>
+      <button kendoButton [disabled]="true">another default button</button>
     </div>
+
+    <button kendoButton [primary]="true">primary button</button>
+    <button kendoButton [togglable]="true">togglable button</button>
   `
 })
 export class AppComponent {
